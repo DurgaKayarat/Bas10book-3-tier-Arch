4510ec1c926262b62cd733d0a38aec5f8fd59167
Durga Kayarat <118466426+DurgaKayarat@users.noreply.github.com>
2024-04-18 12:14:56 -0400

    Update bastionbook.py

--- bastionbook.py
+++ bastionbook.py
@@ -13,7 +13,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!! Durga </p>"
+    return "<p>Hello, World!!! Durga 4/18</p>"
 
 #for passing inputs through extensions
 #the decorator argument and the function argument
